840c93976d090d2ac642bb1226923a298ebfbc31
TimmyTF <timmytf@gmail.com>
2014-09-22 19:03:18 +0400

    Thread variations

---
+++ src/concurrency/the/very/basics/SelfManaged.java
@@ -0,0 +1,33 @@
+package concurrency.the.very.basics;
+
+/**
+ * Created by tflomin on 22.09.2014.
+ *
+ */
+public class SelfManaged implements Runnable {
+    private int countDown = 5;
+    private Thread t = new Thread(this);
+
+    public SelfManaged() {
+        t.start();
+    }
+
+    @Override
+    public String toString() {
+        return Thread.currentThread().getName() + "(" + countDown + "), ";
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println(this);
+            if (--countDown == 0)
+                return;
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++)
+            new SelfManaged();
+    }
+}
--- src/dummypackage/DummyClass.java
+++
@@ -1,9 +0,0 @@
-package dummypackage;
-
-/**
- * Created by tflomin on 26.08.2014.
- *
- */
-public class DummyClass {
-    //
-}
